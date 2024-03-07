85426bfd5d3da66e09e2f38980f98c5bcddd3889
Fahmi Alfareza <falfareza@binaracademy.org>
2024-03-07 20:57:05 +0700

    Chapter 2: topic 5

--- chapter-2/topic-4/scripts/index.js
+++ chapter-2/topic-4/scripts/index.js
@@ -7,6 +7,8 @@ import students from "./students.js";
 const studentCard = document.getElementById("student-card");
 const navbarBrand = document.getElementsByClassName("navbar-brand");
 const searchSubmit = document.getElementById("search-submit");
+const searchInput = document.getElementById("search-input");
+const sortSelect = document.getElementById("sort");
 
 studentCard.innerHTML = "<h1>Loading...</h1>";
 
@@ -33,5 +35,77 @@ setTimeout(() => {
 */
 searchSubmit.addEventListener("submit", (event) => {
     event.preventDefault();
-    alert("Aduh aku malah di-klik!");
+
+    // reset the select option
+    sortSelect.selectedIndex = 0;
+
+    // make a loading
+    studentCard.innerHTML = "<h1>Loading...</h1>";
+
+    // search data
+    const searchedStudents = students.searchStudents(searchInput.value);
+    let studentCardWithStudentsData = "";
+    searchedStudents.map((student) => {
+        studentCardWithStudentsData += `<div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">${student.name}</h5>
+                            <p class="card-text">${student.address.city}, ${student.address.province}</p>
+                        </div>
+                    </div>
+                </div>`;
+    });
+    studentCard.innerHTML = studentCardWithStudentsData;
+});
+
+// Sort select
+sortSelect.addEventListener("change", (event) => {
+    event.preventDefault();
+
+    // Get students data
+    let studentsData = [...students.searchStudents(searchInput.value)];
+
+    // Logic to ascending or descending the data
+    const selectedSort = event.target.value;
+    if (selectedSort == "ascending") {
+        studentsData = studentsData.sort((a, b) => {
+            const nameA = a.name.toUpperCase();
+            const nameB = b.name.toUpperCase();
+            if (nameA < nameB) {
+                return -1;
+            }
+            if (nameA > nameB) {
+                return 1;
+            }
+
+            return 0;
+        });
+    } else if (selectedSort == "descending") {
+        studentsData = studentsData.sort((a, b) => {
+            const nameA = a.name.toUpperCase();
+            const nameB = b.name.toUpperCase();
+            if (nameA > nameB) {
+                return -1;
+            }
+            if (nameA < nameB) {
+                return 1;
+            }
+
+            return 0;
+        });
+    }
+
+    // Make html content that will be displayed
+    let studentCardWithStudentsData = "";
+    studentsData.map((student) => {
+        studentCardWithStudentsData += `<div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">${student.name}</h5>
+                            <p class="card-text">${student.address.city}, ${student.address.province}</p>
+                        </div>
+                    </div>
+                </div>`;
+    });
+    studentCard.innerHTML = studentCardWithStudentsData;
 });
