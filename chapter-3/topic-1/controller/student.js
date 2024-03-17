00a963c485333f2532fd91ff5c7f220253eaaf83
Fahmi Alfareza <falfareza@binaracademy.org>
2024-03-18 06:44:47 +0700

    Chapter 3: topic 5

--- chapter-3/topic-1/controller/student.js
+++ chapter-3/topic-1/controller/student.js
@@ -14,14 +14,14 @@ exports.getStudents = (req, res) => {
     res.status(200).json(response);
 };
 
-exports.getStudent = (req, res) => {
+exports.getStudent = (req, res, next) => {
     const { id } = req.params;
 
     const data = studentUsecase.getStudent(id);
     if (!data) {
-        return res.status(404).json({
-            data: null,
-            message: `Student with id ${id} is not found!`,
+        return next({
+            statusCode: 404,
+            message: `Student with id ${id} is not found `,
         });
     }
 
