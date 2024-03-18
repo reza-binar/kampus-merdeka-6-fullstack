fabd11076e3e241362b0f39ce2e05a7ce07dc7f1
Fahmi Alfareza <falfareza@binaracademy.org>
2024-03-18 07:16:13 +0700

    Chapter 3: topic 5

--- chapter-3/topic-1/route/student.js
+++ chapter-3/topic-1/route/student.js
@@ -3,10 +3,14 @@ const router = express.Router();
 const studentController = require("../controller/student");
 
 /* Add routes */
-router.get("/", studentController.getStudents); // /students
-router.post("/", studentController.addStudent);
-router.get("/:id", studentController.getStudent); // /students/:id
-router.put("/:id", studentController.updateStudent);
-router.delete("/:id", studentController.deleteStudent);
+router
+    .route("/")
+    .get(studentController.getStudents)
+    .post(studentController.addStudent);
+router
+    .route("/:id")
+    .get(studentController.getStudent)
+    .put(studentController.updateStudent)
+    .delete(studentController.deleteStudent);
 
 module.exports = router;
