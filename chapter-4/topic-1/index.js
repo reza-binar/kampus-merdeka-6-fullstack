33697839937e55fe547cbac13f39a35e2588124e
Fahmi Alfareza <falfareza@binaracademy.org>
2024-03-22 07:47:45 +0700

    Chapter 4: topic 2

--- chapter-4/topic-1/index.js
+++ chapter-4/topic-1/index.js
@@ -7,7 +7,25 @@ const port = 4000;
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
+app.get("/classes/:id", async (req, res) => {
+    const { id } = req.params;
+
+    const data = await classes.findAll({
+        where: {
+            id,
+        },
+        include: {
+            model: student,
+        },
+    });
+
+    res.status(200).json({
+        message: "Successs",
+        data,
+    });
+});
+
+app.get("/students", async (req, res) => {
     const data = await student.findAll({
         include: {
             model: classes,
