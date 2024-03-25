8dabd83ea8604353992dd9f8a5f1444573d6e2d4
Fahmi Alfareza <falfareza@binaracademy.org>
2024-03-25 08:07:50 +0700

    Chapter 4: topic 3

--- chapter-4/topic-1/controller/class.js
+++ chapter-4/topic-1/controller/class.js
@@ -1,74 +1,94 @@
 const classUsecase = require("../usecase/class");
 
 exports.getClasses = async (req, res, next) => {
-    const data = await classUsecase.getClasses();
+    try {
+        const data = await classUsecase.getClasses();
 
-    res.status(200).json({
-        message: "Successs",
-        data,
-    });
+        res.status(200).json({
+            message: "Successs",
+            data,
+        });
+    } catch (error) {
+        next(error);
+    }
 };
 
 exports.getClass = async (req, res, next) => {
-    const { id } = req.params;
-    const data = await classUsecase.getClass(id);
-    if (!data) {
-        return next({
-            message: `Class with id ${id} is not found!`,
-            statusCode: 404,
-        });
-    }
+    try {
+        const { id } = req.params;
+        const data = await classUsecase.getClass(id);
+        if (!data) {
+            return next({
+                message: `Class with id ${id} is not found!`,
+                statusCode: 404,
+            });
+        }
 
-    res.status(200).json({
-        message: "Successs",
-        data,
-    });
+        res.status(200).json({
+            message: "Successs",
+            data,
+        });
+    } catch (error) {
+        next(error);
+    }
 };
 
 exports.createClass = async (req, res, next) => {
-    const { name } = req.body;
-    if (!name || name == "") {
-        return next({
-            message: "Name must be provided!",
-            statusCode: 400,
+    try {
+        const { name } = req.body;
+        if (!name || name == "") {
+            return next({
+                message: "Name must be provided!",
+                statusCode: 400,
+            });
+        }
+
+        const data = await classUsecase.createClass({
+            name,
         });
+
+        res.status(201).json({
+            message: "Successs",
+            data,
+        });
+    } catch (error) {
+        next(error);
     }
-
-    const data = await classUsecase.createClass({
-        name,
-    });
-
-    res.status(201).json({
-        message: "Successs",
-        data,
-    });
 };
 
 exports.updateClass = async (req, res, next) => {
-    const { id } = req.params;
-    const { name } = req.body;
-    if (!name || name == "") {
-        return next({
-            message: "Name must be provided!",
-            statusCode: 400,
+    try {
+        const { id } = req.params;
+        const { name } = req.body;
+        if (!name || name == "") {
+            return next({
+                message: "Name must be provided!",
+                statusCode: 400,
+            });
+        }
+
+        const data = await classUsecase.updateClass(id, { name });
+
+        res.status(200).json({
+            message: "Successs",
+            data,
         });
+    } catch (error) {
+        next(error);
     }
-
-    const data = await classUsecase.updateClass(id, { name });
-
-    res.status(200).json({
-        message: "Successs",
-        data,
-    });
 };
 
 exports.deleteClass = async (req, res, next) => {
-    const { id } = req.params;
+    try {
+        const { id } = req.params;
 
-    const data = await classUsecase.deleteClass(id);
+        const data = await classUsecase.deleteClass(id);
 
-    res.status(200).json({
-        message: "Successs",
-        data,
-    });
+        res.status(200).json({
+            message: "Successs",
+            data,
+        });
+    } catch (error) {
+        next(error);
+    }
 };
