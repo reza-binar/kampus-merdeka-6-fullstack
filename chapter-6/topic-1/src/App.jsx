b63a4c58faec2605fc3439fe410a9a560cd62606
Fahmi Alfareza <falfareza@binaracademy.org>
2024-04-22 21:29:50 +0700

    chapter 6: 1

---
+++ chapter-6/topic-1/src/App.jsx
@@ -0,0 +1,49 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Login from "./pages/login";
+
+import "bootstrap/dist/css/bootstrap.min.css"; // apply bootstrap for styling
+import Container from "react-bootstrap/Container";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: (
+            <>
+                <Navbar />
+                <Container className="mt-5">
+                    <h1>Home</h1>
+                </Container>
+            </>
+        ),
+    },
+    {
+        path: "/login",
+        element: (
+            <>
+                <Navbar />
+                <Container className="mt-5">
+                    <Login />
+                </Container>
+            </>
+        ),
+    },
+    {
+        path: "/register",
+        element: (
+            <>
+                <Navbar />
+                <Container className="mt-5">
+                    <h1>Register</h1>
+                </Container>
+            </>
+        ),
+    },
+]);
+
+function App() {
+    return <RouterProvider router={router} />;
+}
+
+export default App;
